1808b99edbb0d863fd53262dfb3bc91b7d0fef6a
MattC <mattc@MattCs-MacBook-Air.local>
2025-08-28 10:53:01 -0400

    Quartz sync: Aug 28, 2025, 10:53 AM

--- quartz/plugins/emitters/componentResources.ts
+++ quartz/plugins/emitters/componentResources.ts
@@ -106,23 +106,6 @@ function addGlobalPageResources(ctx: BuildCtx, componentResources: ComponentReso
       
       document.head.appendChild(gtagScript);
     `)
-  } else if (cfg.analytics?.provider === "plausible") {
-    const plausibleHost = cfg.analytics.host ?? "https://plausible.io"
-    componentResources.afterDOMLoaded.push(`
-      const plausibleScript = document.createElement('script');
-      plausibleScript.src = '${plausibleHost}/js/script.manual.js';
-      plausibleScript.setAttribute('data-domain', location.hostname);
-      plausibleScript.defer = true;
-      plausibleScript.onload = () => {
-        window.plausible = window.plausible || function () { (window.plausible.q = window.plausible.q || []).push(arguments); };
-        plausible('pageview');
-        document.addEventListener('nav', () => {
-          plausible('pageview');
-        });
-      };
-
-      document.head.appendChild(plausibleScript);
-    `)
   } else if (cfg.analytics?.provider === "umami") {
     componentResources.afterDOMLoaded.push(`
       const umamiScript = document.createElement("script");
